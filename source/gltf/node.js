007371c424be973371077774bd4ccd4bdb3d2dca
Patrick Härtl <haertl@ux3d.io>
2024-08-20 14:52:15 +0200

    Fix matrix in node

--- source/gltf/node.js
+++ source/gltf/node.js
@@ -1,4 +1,4 @@
-import { mat4 } from 'gl-matrix';
+import { mat4, quat } from 'gl-matrix';
 import { jsToGl } from './utils.js';
 import { GltfObject } from './gltf_object.js';
 
@@ -19,6 +19,7 @@ class gltfNode extends GltfObject
         super();
         this.camera = undefined;
         this.children = [];
+        this.matrix = undefined;
         this.rotation = jsToGl([0, 0, 0, 1]);
         this.scale = jsToGl([1, 1, 1]);
         this.translation = jsToGl([0, 0, 0]);
@@ -34,10 +35,11 @@ class gltfNode extends GltfObject
         this.light = undefined;
     }
 
-    fromJson(json)
-    {
-        super.fromJson(json);
-        //makeAnimatable(this, json, { "weights": [] });
+    fromJson(jsonNode) {
+        super.fromJson(jsonNode);
+        if (jsonNode.matrix !== undefined) {
+            this.applyMatrix(jsonNode.matrix);
+        }
     }
 
     getWeights(gltf)
@@ -50,32 +52,24 @@ class gltfNode extends GltfObject
         }
     }
 
-    // TODO: Not called. What about nodes which only define a matrix?
     applyMatrix(matrixData)
     {
-        // this.matrix = jsToGl(matrixData);
+        this.matrix = jsToGl(matrixData);
 
-        // mat4.getScaling(this.scale, this.matrix);
+        mat4.getScaling(this.scale, this.matrix);
 
-        // // To extract a correct rotation, the scaling component must be eliminated.
-        // const mn = mat4.create();
-        // for(const col of [0, 1, 2])
-        // {
-        //     mn[col] = this.matrix[col] / this.scale[0];
-        //     mn[col + 4] = this.matrix[col + 4] / this.scale[1];
-        //     mn[col + 8] = this.matrix[col + 8] / this.scale[2];
-        // }
-        // mat4.getRotation(this.rotation, mn);
-        // quat.normalize(this.rotation, this.rotation);
+        // To extract a correct rotation, the scaling component must be eliminated.
+        const mn = mat4.create();
+        for(const col of [0, 1, 2])
+        {
+            mn[col] = this.matrix[col] / this.scale[0];
+            mn[col + 4] = this.matrix[col + 4] / this.scale[1];
+            mn[col + 8] = this.matrix[col + 8] / this.scale[2];
+        }
+        mat4.getRotation(this.rotation, mn);
+        quat.normalize(this.rotation, this.rotation);
 
-        // mat4.getTranslation(this.translation, this.matrix);
-    }
-
-    resetTransform()
-    {
-        this.animatedPropertyObjects["rotation"].rest();
-        this.animatedPropertyObjects["scale"].rest();
-        this.animatedPropertyObjects["translation"].rest();
+        mat4.getTranslation(this.translation, this.matrix);
     }
 
     getLocalTransform()
