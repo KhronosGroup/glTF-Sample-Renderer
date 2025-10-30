bc532c55f5e6b5ce4ffbfc4b43248c1c84a20de6
Patrick Härtl <haertl@ux3d.io>
2025-10-30 15:23:24 +0100

    Move test in function

--- tests/interactivityTests.spec.ts
+++ tests/interactivityTests.spec.ts
@@ -35,75 +35,79 @@ for (const dir of directories) {
         const path = asset.path;
         const file = new Uint8Array(fs.readFileSync(path));
         const testName = path.substring(path.lastIndexOf("/testAssetDownloads/") + 19);
-        test(`Testing asset ${testName}`, async ({ page }) => {
-            await page.goto("");
-            let testDuration: number | undefined = undefined;
-            let testResult: boolean | undefined = undefined;
-            const fun = (input: number | boolean) => {
-                if (typeof input === "number") {
-                    testDuration = input;
-                } else if (typeof input === "boolean") {
-                    testResult = input;
-                }
-            };
-            await page.exposeFunction("passTestData", fun);
-            let success = false;
-            try {
-                success = await page.evaluate(async (file) => {
-                    const resourceLoader = window.resourceLoader as ResourceLoader;
-                    const state = window.state as GltfState;
-                    const glTF = await resourceLoader.loadGltf(file.buffer);
-                    state.gltf = glTF;
-                    const defaultScene = state.gltf.scene;
-                    state.sceneIndex = defaultScene === undefined ? 0 : defaultScene;
-                    state.cameraNodeIndex = undefined;
-                    state.graphController.addCustomEventListener("test/onStart", (event) => {
-                        window.passTestData(event.detail.expectedDuration);
-                        window.TEST_TIME = event.detail.expectedDuration;
-                    });
-                    state.graphController.addCustomEventListener("test/onSuccess", () => {
-                        window.passTestData(true);
-                        window.TEST_RESULT = true;
-                    });
-                    state.graphController.addCustomEventListener("test/onFailed", () => {
-                        window.passTestData(false);
-                        window.TEST_RESULT = false;
-                    });
-                    state.animationTimer.start();
-                    if (state.gltf?.extensions?.KHR_interactivity?.graphs !== undefined) {
-                        state.graphController.initializeGraphs(state);
-                        const graphIndex = state.gltf.extensions.KHR_interactivity.graph ?? 0;
-                        state.graphController.loadGraph(graphIndex);
-                        state.graphController.resumeGraph();
-                    } else {
-                        state.graphController.stopGraphEngine();
-                    }
-                    return true;
-                }, file);
-            } catch (error) {
-                console.log(await page.consoleMessages());
-                throw error;
-            }
-            expect(success).toBeTruthy();
-            await page.waitForFunction(
-                () => {
-                    return window.TEST_TIME !== undefined;
-                },
-                { timeout: 2000 }
-            );
-            if (testDuration! > 0) {
-                console.log("Test duration (s): ", testDuration);
-            }
-            await page.waitForFunction(
-                () => {
-                    return window.TEST_RESULT !== undefined;
-                },
-                { timeout: testDuration! * 1000 + 1000 }
-            );
-            if (testResult === false) {
-                console.log(await page.consoleMessages());
-            }
-            expect(testResult).toBe(true);
-        });
+        createInteractivityTest(testName, file);
     }
+}
+
+function createInteractivityTest(name: string, file: Uint8Array<ArrayBuffer>) {
+    test(`Testing asset ${name}`, async ({ page }) => {
+        await page.goto("");
+        let testDuration: number | undefined = undefined;
+        let testResult: boolean | undefined = undefined;
+        const fun = (input: number | boolean) => {
+            if (typeof input === "number") {
+                testDuration = input;
+            } else if (typeof input === "boolean") {
+                testResult = input;
+            }
+        };
+        await page.exposeFunction("passTestData", fun);
+        let success = false;
+        try {
+            success = await page.evaluate(async (file) => {
+                const resourceLoader = window.resourceLoader as ResourceLoader;
+                const state = window.state as GltfState;
+                const glTF = await resourceLoader.loadGltf(file.buffer);
+                state.gltf = glTF;
+                const defaultScene = state.gltf.scene;
+                state.sceneIndex = defaultScene === undefined ? 0 : defaultScene;
+                state.cameraNodeIndex = undefined;
+                state.graphController.addCustomEventListener("test/onStart", (event) => {
+                    window.passTestData(event.detail.expectedDuration);
+                    window.TEST_TIME = event.detail.expectedDuration;
+                });
+                state.graphController.addCustomEventListener("test/onSuccess", () => {
+                    window.passTestData(true);
+                    window.TEST_RESULT = true;
+                });
+                state.graphController.addCustomEventListener("test/onFailed", () => {
+                    window.passTestData(false);
+                    window.TEST_RESULT = false;
+                });
+                state.animationTimer.start();
+                if (state.gltf?.extensions?.KHR_interactivity?.graphs !== undefined) {
+                    state.graphController.initializeGraphs(state);
+                    const graphIndex = state.gltf.extensions.KHR_interactivity.graph ?? 0;
+                    state.graphController.loadGraph(graphIndex);
+                    state.graphController.resumeGraph();
+                } else {
+                    state.graphController.stopGraphEngine();
+                }
+                return true;
+            }, file);
+        } catch (error) {
+            console.log(await page.consoleMessages());
+            throw error;
+        }
+        expect(success).toBeTruthy();
+        await page.waitForFunction(
+            () => {
+                return window.TEST_TIME !== undefined;
+            },
+            { timeout: 2000 }
+        );
+        if (testDuration! > 0) {
+            console.log("Test duration (s): ", testDuration);
+        }
+        await page.waitForFunction(
+            () => {
+                return window.TEST_RESULT !== undefined;
+            },
+            { timeout: testDuration! * 1000 + 1000 }
+        );
+        if (testResult === false) {
+            console.log(await page.consoleMessages());
+        }
+        expect(testResult).toBe(true);
+    });
 }
