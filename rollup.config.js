8fc0b44f88de8d45837e8a82d884ef7e5ba10515
Jim Eckerlein <eckerlein@ux3d.io>
2026-09-22 15:16:21 +0300

    Externalize gl-matrix to avoid duplicate bundling in consumers
    Consumers that already depend on gl-matrix directly (e.g. glTF-Sample-Viewer)
    would otherwise get a second inlined copy of it in their final bundle.

--- rollup.config.js
+++ rollup.config.js
@@ -14,6 +14,7 @@ export default {
         warn(warning);
     },
     input: ['source/gltf-sample-renderer.js'],
+    external: ['gl-matrix'],
     output: [
         {
             file: 'dist/gltf-viewer.js',
